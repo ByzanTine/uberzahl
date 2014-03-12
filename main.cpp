31c0ebf5eb3e617708144f5ac849749299dfc0e0
sai <sai@email.com>
2014-03-12 12:43:50 -0400

    comparison operators are working as expected.

--- main.cpp
+++ main.cpp
@@ -8,4 +8,31 @@ int main( void ){
   uberzahl b = "5";
   cout << (a + 1);
   cout << (a + 1) * 2;
+
+  uberzahl c = "1234";
+  uberzahl d = "5678";
+  uberzahl e = "123456789123456789";
+  uberzahl f = "123456789123456789"; 
+
+  if (c == d) cout << "c == d" << endl; //won't print
+  if (e == f) cout << "e == f" << endl; //will print
+  if (c < d) cout << "c < d" << endl; //will print
+  if (c <= d) cout << "c <= d" << endl; //will print
+  if (e > c) cout << "e > c" << endl; //will print
+  if (f >= e) cout << "f >= e" << endl; //will print
+  if (d >= f) cout << "d >= f" << endl; //won't print
+
+  uberzahl g = "0000000000000000000000000000000000000000123456";
+  uberzahl h = "123456"; 
+  uberzahl i = "123457"; 
+
+  if (g == h) cout << "g == h" << endl; //will print
+  if (g >= h) cout << "g >= h" << endl; //will print
+  if (i >= g) cout << "i >= g" << endl; //will print
+
+  uberzahl j = "1239808213902318901089231283901238904392834982374"; 
+  uberzahl k = "42949672964294967296"; 
+  cout << k; 
+
 }
+
